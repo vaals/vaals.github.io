c92e047b0cd024cf17b305497b2b59b65c5803b4
vaals <efreeti@bk.ru>
2017-01-28 14:18:11 +0200

    Revert "hotels test"
    This reverts commit 8b1158846cf025ff30f1d139e5db8e0bb5c08e08.

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-gulp.task('less', function () {
-    gulp.src('./build/less/**/*.less').pipe(less()).pipe(gulp.dest('./build/css'));
-});
-gulp.task('watch', function () {
-    gulp.watch('./build/less/**/*less', ['less']);
-});
